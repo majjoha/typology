6d4bf0b2c5719b4c74cf7b1ea91ba6bc3fb3e9ee
Mathias Jean Johansen <mathias@mjj.io>
2021-03-31 14:46:21 +0200

    Add type for adding two numbers.

--- src/Arithmetic.ts
+++ src/Arithmetic.ts
@@ -1,5 +1,11 @@
 import type { Zero, Succ } from "./Nat"
+import type { IfElse, IsZero } from "./Conditionals"
 
 export type Increment<N> = Succ<N>
 
 export type Decrement<N> = N extends Succ<infer R> ? R : Zero
+
+export type Add<A, B> = {
+  acc: B
+  n: A extends Succ<infer _> ? Add<Decrement<A>, Succ<B>> : never
+}[IfElse<IsZero<A>, "acc", "n">]
--- src/__tests__/Arithmetic.ts
+++ src/__tests__/Arithmetic.ts
@@ -1,5 +1,5 @@
 import type { Succ, Zero } from "../Nat"
-import type { Increment, Decrement } from "../Arithmetic"
+import type { Increment, Decrement, Add } from "../Arithmetic"
 
 describe("Increment", () => {
   it("increments a number", () => {
@@ -34,6 +34,57 @@ describe("Decrement", () => {
       },
     }
     const two: Succ<Succ<Zero>> = { n: { n: "zero" } }
+
+    expect(result).toStrictEqual(two)
+  })
+})
+
+describe("Add", () => {
+  it("returns `Zero` when adding two `Zero`s", () => {
+    const result: Add<Zero, Zero> = "zero"
+    const zero: Zero = "zero"
+
+    expect(result).toBe(zero)
+  })
+
+  it("returns five when adding two and three", () => {
+    const result: Add<Succ<Succ<Zero>>, Succ<Succ<Succ<Zero>>>> = {
+      n: {
+        n: {
+          n: {
+            n: {
+              n: "zero",
+            },
+          },
+        },
+      },
+    }
+    const five: Succ<Succ<Succ<Succ<Succ<Zero>>>>> = {
+      n: {
+        n: {
+          n: {
+            n: {
+              n: "zero",
+            },
+          },
+        },
+      },
+    }
+
+    expect(result).toStrictEqual(five)
+  })
+
+  it("returns 2 when adding 2 and `Zero`", () => {
+    const result: Add<Zero, Succ<Succ<Zero>>> = {
+      n: {
+        n: "zero",
+      },
+    }
+    const two: Succ<Succ<Zero>> = {
+      n: {
+        n: "zero",
+      },
+    }
 
     expect(result).toStrictEqual(two)
   })
