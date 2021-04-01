28accd690d683d799a87d62065797432811cfa5c
Mathias Jean Johansen <mathias@mjj.io>
2021-04-01 09:11:45 +0200

    Add type for converting strings into numbers.

--- src/Parse.ts
+++ src/Parse.ts
@@ -5,3 +5,9 @@ export type ToString<T, A extends string = Zero> = {
   acc: `${A}`
   n: T extends Succ<infer T1> ? ToString<T1, `{ n: ${A} }`> : never
 }[IfElse<IsZero<T>, "acc", "n">]
+
+export type FromString<S extends string = Zero> = S extends `{ n: ${infer S1} }`
+  ? Succ<FromString<S1>>
+  : S extends `zero`
+  ? Zero
+  : Zero
